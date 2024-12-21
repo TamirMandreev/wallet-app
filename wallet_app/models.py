44d4a66132f8160dd5d6aff2280847e5f32f7ee8
Tamir Mandreev <tamirmandreev@mail.ru>
2024-12-21 19:35:44 +0800

    Переопределил метод __str__ модели Wallet. Теперь метод возвращает удобочитаемую информацию об объекте класса

--- wallet_app/models.py
+++ wallet_app/models.py
@@ -8,3 +8,8 @@ class Wallet(models.Model):
     uuid = models.UUIDField(primary_key=True)
     # Баланс
     balance = models.DecimalField(max_digit=20, decimal_places=2, default=0)
+
+    # Переопределить метод __str__, чтобы он возвращал удобочитаемую информацию
+    # об объекте класса
+    def __str__(self):
+        return f'Кошелек {self.uuid}: Баланс {self.balance}'
