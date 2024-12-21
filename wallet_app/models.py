af0eb7bb47b2ffd23a6016fbc2e9af48015c7b1c
Tamir Mandreev <tamirmandreev@mail.ru>
2024-12-21 19:31:45 +0800

    Создал модель Wallet. В ней будут храниться данные о кошельке

--- wallet_app/models.py
+++ wallet_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# Создать модель для кошелька
+class Wallet(models.Model):
+    # uuid - универсально уникальный идентификатор
+    uuid = models.UUIDField(primary_key=True)
+    # Баланс
+    balance = models.DecimalField(max_digit=20, decimal_places=2, default=0)
